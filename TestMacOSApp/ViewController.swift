b1d8629414ae46e32bc4656903c9edfe09b7bd6d
Vladislav <v.shtytcko@softteco.com>
2020-11-03 11:11:29 +0300

    Initial Commit

---
+++ TestMacOSApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestMacOSApp
+//
+//  Created by Vladislav on 11/3/20.
+//  Copyright © 2020 Vladislav. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
